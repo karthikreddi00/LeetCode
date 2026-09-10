d3a7faa9c3ef08e5372cbbe15ebb750501fd9320
karthik reddy <166686763+karthikreddi00@users.noreply.github.com>
2026-09-10 19:24:02 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (24.23%) - LeetSync

---
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.java
@@ -0,0 +1,32 @@
+class Solution {
+
+    static int count = 0;
+
+    public int[] dfs(TreeNode root) {
+
+        if (root == null) {
+            return new int[]{0, 0};
+        }
+
+        int[] left = dfs(root.left);
+        int[] right = dfs(root.right);
+
+        int sum = left[0] + right[0] + root.val;
+        int nodes = left[1] + right[1] + 1;
+
+        if (sum / nodes == root.val) {
+            count++;
+        }
+
+        return new int[]{sum, nodes};
+    }
+
+    public int averageOfSubtree(TreeNode root) {
+
+        count = 0;
+
+        dfs(root);
+
+        return count;
+    }
+}
